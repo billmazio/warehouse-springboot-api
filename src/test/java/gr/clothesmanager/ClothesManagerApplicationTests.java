08c9bfc37f1fbe241657b19a6e1e665c87a8221f
vasilis <bmaziotis@windowslive.com>
2025-10-25 17:04:22 +0300

    Add first playwright test

--- src/test/java/gr/clothesmanager/ClothesManagerApplicationTests.java
+++ src/test/java/gr/clothesmanager/ClothesManagerApplicationTests.java
@@ -1,5 +1,9 @@
 package gr.clothesmanager;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,17 @@ class ClothesManagerApplicationTests {
 
     @Test
     void contextLoads() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("http://localhost:3000/login");
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Warehouse Management System"));
+
+        browser.close();
+        playwright.close();
     }
 
 }
