312d464911b9406002ed7899aed938761cdb4b12
vasilis <bmaziotis@windowslive.com>
2025-11-17 11:49:27 +0200

    Minor fixes

--- src/test/java/gr/clothesmanager/tests/MaterialCreateTests.java
+++ src/test/java/gr/clothesmanager/tests/MaterialCreateTests.java
@@ -13,43 +13,25 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static gr.clothesmanager.helpers.AuthenticationHelper.loginAsAdmin;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 @UsePlaywright(HeadlessChromeOptions.class)
 public class MaterialCreateTests {
-    
-    @Test
-    @Order(1)
-    @DisplayName("Should add material successfully")
-    public void shouldAddMaterialSuccessfully(Page page) {
-        DashboardPage dashboardPage = loginAsAdmin(page);
-        MaterialsPage materialsPage = dashboardPage.navigateToMaterials();
-        materialsPage.waitForLoad();
-        
-        materialsPage.addMaterial(
-            "Μπλούζα Polo",
-            TestConstants.SIZE_MEDIUM,
-            "10",
-            TestConstants.STORE_KENTRIKA
-        );
-        
-        assertFalse(materialsPage.isAddMaterialModalVisible(),
-            "Add material modal should be hidden after submission");
-    }
-    
+
     @ParameterizedTest
-    @Order(2)
     @ValueSource(strings = {"EXTRA SMALL", "SMALL", "MEDIUM", "LARGE", "EXTRA LARGE"})
     @DisplayName("Should add materials with different sizes")
     public void shouldAddMaterialsWithDifferentSizes(String size, Page page) {
         DashboardPage dashboardPage = loginAsAdmin(page);
         MaterialsPage materialsPage = dashboardPage.navigateToMaterials();
         materialsPage.waitForLoad();
-        
+
+        String uniqueName = "Μπλούζα_" + System.currentTimeMillis();
+
         materialsPage.addMaterial(
-            "Μπλούζα",
-            size,
-            "10",
-            TestConstants.STORE_KENTRIKA
+                uniqueName,
+                size,
+                "10",
+                TestConstants.STORE_KENTRIKA
         );
         
         assertFalse(materialsPage.isAddMaterialModalVisible(),
