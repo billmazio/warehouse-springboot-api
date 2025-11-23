edd72cd8d2c56f0035e1d893dbc0890120bfe474
vasilis <bmaziotis@windowslive.com>
2025-11-23 16:03:30 +0200

    Add backend and frontend startup to workflows, enable headless mode for CI/CD

--- src/test/java/gr/clothesmanager/config/HeadlessChromeOptions.java
+++ src/test/java/gr/clothesmanager/config/HeadlessChromeOptions.java
@@ -9,10 +9,12 @@ import java.util.Arrays;
 public class HeadlessChromeOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
+        boolean isHeadless = System.getenv("CI") != null || System.getenv("GITHUB_ACTIONS") != null;
+
         return new Options().setLaunchOptions(
                         new BrowserType.LaunchOptions()
                                 .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
-                ).setHeadless(true)
+                ).setHeadless(isHeadless)
                 .setTestIdAttribute("data-test");
     }
 }
